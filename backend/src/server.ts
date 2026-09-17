a8e6b5c77f3be99891260df859bf3afc87ebcaec
“Monika <3027923t@student.gla.ac.uk>
2026-09-17 22:55:52 +0100

    server.ts change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
-import 'dotenv/config';
 import app from './app';
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:3000`);
+  console.log(`Server is running on port ${port}`);
 });
